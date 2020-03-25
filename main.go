68645f1e00a11b1c3a5e43d1c16d4b28ed95c458
Peter <44655627+rxprep-psjolin@users.noreply.github.com>
2020-03-25 07:13:22 -0700

    headers

--- main.go
+++ main.go
@@ -6,6 +6,9 @@ func main() {
 	mortality := 0.05
 	population := float64(327e6)
 
+	fmt.Println("Population:", fmt.Sprintf("%6.2f", population))
+	fmt.Println("Mortality:", mortality*100, "%")
+
 	for probability := float64(0.0); probability <= 100; probability += 5 {
 		percent := fmt.Sprintf("%0.2f", probability)
 		fmt.Println("With probability of:", percent, "%")
