aa8bc7723d30b58bf407d1822c380e7e678b5750
Peter <44655627+rxprep-psjolin@users.noreply.github.com>
2020-03-25 19:45:22 -0700

    cleanup

--- main.go
+++ main.go
@@ -12,7 +12,6 @@ func main() {
 
 	w := new(tabwriter.Writer)
 	w.Init(os.Stdout, 8, 8, 0, '\t', 0)
-
 	defer w.Flush()
 
 	fmt.Println("US Population:", fmt.Sprintf("%6.0f", population))
@@ -20,11 +19,10 @@ func main() {
 
 	fmt.Println("Average Mortality Rate:", mortalityPercent, "%")
 
-	fmt.Fprintf(w, "\n %s\t%s\t", "Infection\nprobability %", "#Dead")
+	fmt.Fprintf(w, "\n %s\t%s\t", "% Infection\nprobability", "#Dead")
 
 	for probability := float64(0.0); probability <= 100; probability += 5 {
 		dead := population * probability / 100 * mortality
-		//fmt.Println(" Number dead:", dead)
 		fmt.Fprintf(w, "\n %0.2f\t%6.0f\t", probability, dead)
 	}
 }
